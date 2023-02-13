23e74eefb47bb80e275415f509ad11a590be87ba
NIHALPATTANSHETTY <80695465+NIHALPATTANSHETTY@users.noreply.github.com>
2023-02-13 22:24:19 +0530

    Delete temp.cpp

--- Dockerfile/temp.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(void) {
-  cout << "Hello, World I am NIHALPATTANSHETTY\n";
-  cout << "I am in Task5 my SRN is PES1UG20CS266\n";
-  return 0;
-}
